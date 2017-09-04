e3552286c2625852180274d3c52ed8c11f3d9a08
zackslay3r <zackary.direen@hotmail.com>
2017-09-04 17:11:27 +1000

    Init

---
+++ PlayAround/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float forward;
+    public float rotation;
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody>().velocity += transform.forward * forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().velocity += -transform.forward * forward * Time.deltaTime;
+        }
+
+
+
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotation;
+
+        transform.Rotate(0, x, 0);
+      
+    }
+}
---
+++ PlayAround/Assets/goal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goal : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
